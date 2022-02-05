52c182e8e5bf225ca46098272cedc2b73b82417b
parikshit3000 <parikshitjuneja22@gmail.com>
2022-02-05 20:36:30 +0530

    Hello

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get("/about", (req, res) => {
   res.render("about", { abouttext: aboutContent });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, function() {
   console.log("Server started on port " + port);
